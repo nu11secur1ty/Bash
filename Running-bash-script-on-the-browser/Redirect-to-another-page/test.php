bd60ccb0709e164e16af60e2e4c08d332dc03cca
nu11secur1ty <venvaropt@gmail.com>
2023-10-30 09:14:20 +0200

    Delete Running-bash-script-on-the-browser/Redirect-to-another-page/test.php

--- Running-bash-script-on-the-browser/Redirect-to-another-page/test.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$home_url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/finish.html';
-$output = shell_exec('./bash_script.sh');
-header('Location: ' . $home_url);
-?>
