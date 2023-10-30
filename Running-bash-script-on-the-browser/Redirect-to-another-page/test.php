9c46421552304b626449667d72bce46a822d54f6
nu11secur1ty <venvaropt@gmail.com>
2023-10-30 09:15:12 +0200

    Create test.php

---
+++ Running-bash-script-on-the-browser/Redirect-to-another-page/test.php
@@ -0,0 +1,5 @@
+<?php
+$home_url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/finish.html';
+$output = shell_exec('./bash_script.sh');
+header('Location: ' . $home_url);
+?>
